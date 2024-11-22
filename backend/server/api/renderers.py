1d6292a43d2511d9dafbf8ac6637d937122310e6
gavinlin24 <gavinlin24@gmail.com>
2024-11-22 23:55:28 +0900

    Fix XML Tag
    Fixed bug where some closing tags in the resulting XML was incorrect

--- backend/server/api/renderers.py
+++ backend/server/api/renderers.py
@@ -1,15 +1,42 @@
 from rest_framework_xml.renderers import XMLRenderer
+from django.utils.encoding import force_str
 
 class CustomXMLRenderer(XMLRenderer):
     root_tag_name = 'Data'
 
-    def render(self, data, accepted_media_type=None, renderer_context=None):
-        xml = super().render(data, accepted_media_type, renderer_context)
-        # Replace 'list-item' in different contexts with desired names
-        xml = xml.replace('<list-item>', '<Patient>')
-        xml = xml.replace('</list-item>', '</Patient>')
-        xml = xml.replace('<TestData><Patient>', '<TestData><TestDemographics>')
-        xml = xml.replace('</Patient></TestData>', '</TestDemographics></TestData>')
-        xml = xml.replace('<ECGData><Patient>', '<ECGData><ECGMeasurements>')
-        xml = xml.replace('</Patient></ECGData>', '</ECGMeasurements></ECGData>')
-        return xml
+    def _to_xml(self, xml, data):
+        if isinstance(data, (list, tuple)):
+            for item in data:
+                xml.startElement("Patient", {})
+                self._to_xml(xml, item)
+                xml.endElement("Patient")
+
+        elif isinstance(data, dict):
+            for key, value in data.items():
+                if key == "TestData":
+                    xml.startElement("TestData", {})
+                    if isinstance(value, list):
+                        for demo in value:
+                            xml.startElement("TestDemographics", {})
+                            self._to_xml(xml, demo)
+                            xml.endElement("TestDemographics")
+                    xml.endElement("TestData")
+                elif key == "ECGData":
+                    xml.startElement("ECGData", {})
+                    if isinstance(value, list):
+                        for measurement in value:
+                            xml.startElement("ECGMeasurements", {})
+                            self._to_xml(xml, measurement)
+                            xml.endElement("ECGMeasurements")
+                    xml.endElement("ECGData")
+                else:
+                    xml.startElement(key, {})
+                    self._to_xml(xml, value)
+                    xml.endElement(key)
+
+        elif data is None:
+            # Don't output any value for None
+            pass
+
+        else:
+            xml.characters(str(data))
