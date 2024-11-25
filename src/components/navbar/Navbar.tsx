6baecabea5a8bf91e0a2726cdbad980d32d73356
Jackson Yan <jacksoy@uci.edu>
2024-11-25 12:40:07 +0900

    data query interface

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import "./Navbar.css";
+import watermark from "../../assets/images/watermark.png";
+import SearchButton from "./SearchButton";
+
+interface Props{
+  onLogoClick: () => void;
+  onSearchClick: () => void;
+}
+const Navbar = ({onLogoClick, onSearchClick}: Props) => {
+  return (
+    <div className="navbar">
+      <div>
+        
+        <img src={watermark} alt="" className="logo-icon" onClick={onLogoClick}/>
+        
+      </div>
+
+      {/* <ul>
+        <li>Home</li>
+        <li>About</li>
+      </ul> */}
+
+      <SearchButton onClick={onSearchClick}></SearchButton>
+    </div>
+  );
+};
+
+export default Navbar;
