f2043fb14e8998232cf03caa090b8821d1001dbc
jacksonyjh <jxcksxnpersonal@gmail.com>
2024-12-03 14:18:53 +0900

    searchwindow collects data from search input

--- src/components/SearchWindow.tsx
+++ src/components/SearchWindow.tsx
@@ -14,84 +14,120 @@ import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
 const SearchWindow = () => {
+  const [patientId, setPatientId] = useState("");
+  const [patientName, setPatientName] = useState("");
   const [gender, setGender] = useState("");
   const [acqDate, setAcqDate] = useState(null);
+  const [prInterval, setPrInterval] = useState("");
+  const [qtInterval, setQtInterval] = useState("");
+  const [ventricleRate, setVentricleRate] = useState("");
 
-  const handleGenderClick = (selectedGender: string) => {
-    console.log({ selectedGender });
+  const handleGenderClick = (selectedGender) => {
     setGender(selectedGender);
   };
 
-  const handleDateChange = (date: Date) => {
+  const handleDateChange = (date) => {
     setAcqDate(date);
-    console.log(date);
+  };
+
+  const handleSearchClick = () => {
+    const searchData = {
+      patientId,
+      patientName,
+      gender,
+      acqDate,
+      prInterval,
+      qtInterval,
+      ventricleRate,
+    };
+    console.log("Search Data:", searchData);
   };
 
   return (
     <div>
-      <div>
-        {" "}
-        Patient ID <Input placeholder="Enter Patient ID " height="25px"></Input>
-      </div>
-      <div>
-        {" "}
-        Patient Name{" "}
-        <Input placeholder="Enter Patient Name" height="25px"></Input>{" "}
-      </div>
-      <div>
-        {" "}
-        Gender
-        <Stack direction="row" justify="center">
-          {["Male", "Female", "Other"].map((genderOption) => (
-            <Button
-              key={genderOption}
-              className="gen btn"
-              onClick={() => handleGenderClick(genderOption)}
-              style={{
-                backgroundColor:
-                  gender === genderOption ? "lightblue" : "white",
-              }}
+      <form>
+        <div>
+          Patient ID
+          <Input
+            placeholder="Enter Patient ID"
+            height="25px"
+            value={patientId}
+            onChange={(e) => setPatientId(e.target.value)}
+          />
+        </div>
+        <div>
+          Patient Name
+          <Input
+            placeholder="Enter Patient Name"
+            height="25px"
+            value={patientName}
+            onChange={(e) => setPatientName(e.target.value)}
+          />
+        </div>
+        <div>
+          Gender
+          <Stack direction="row" justify="center">
+            {["Male", "Female", "Other"].map((genderOption) => (
+              <Button
+                key={genderOption}
+                className="gen btn"
+                onClick={() => handleGenderClick(genderOption)}
+                style={{
+                  backgroundColor:
+                    gender === genderOption ? "lightblue" : "white",
+                }}
+              >
+                {genderOption}
+              </Button>
+            ))}
+          </Stack>
+        </div>
+        <div>Acquisition Date</div>
+        <DatePicker
+          selected={acqDate}
+          onChange={handleDateChange}
+          placeholderText="Please Select a Date"
+        />
+        <div>PR Interval</div>
+        <Flex justifyContent="center" alignItems="center">
+          <Box>
+            <NumberInput
+              value={prInterval}
+              onChange={(valueString) => setPrInterval(valueString)}
+              width="200px"
             >
-              {genderOption}
-            </Button>
-          ))}
-        </Stack>
-      </div>
-
-      <div> Acquisition Date</div>
-      {/* <Button onClick={()=> setCalendarVis(true)}> Choose Date</Button> */}
-      <DatePicker
-        selected={acqDate}
-        onChange={handleDateChange}
-        placeholderText="Please Select a Date"
-      ></DatePicker>
-      <div> PR Interval</div>
-      <Flex justifyContent="center" alignItems="center">
-        <Box>
-          <NumberInput defaultValue={10} width="200px">
-            <NumberInputField />
-          </NumberInput>
-        </Box>
-      </Flex>
-      <div> QT Interval</div>
-      <Flex justifyContent="center" alignItems="center">
-        <Box>
-          <NumberInput defaultValue={10} width="200px">
-            <NumberInputField />
-          </NumberInput>
-        </Box>
-      </Flex>
-      <div> Ventricle Rate</div>
-      <Flex justifyContent="center" alignItems="center">
-        <Box>
-          <NumberInput defaultValue={10} width="200px">
-            <NumberInputField />
-          </NumberInput>
-        </Box>
-      </Flex>
-      <button type="button" className="btn">
-        Search
-      </button>
+              <NumberInputField />
+            </NumberInput>
+          </Box>
+        </Flex>
+        <div>QT Interval</div>
+        <Flex justifyContent="center" alignItems="center">
+          <Box>
+            <NumberInput
+              value={qtInterval}
+              onChange={(valueString) => setQtInterval(valueString)}
+              width="200px"
+            >
+              <NumberInputField />
+            </NumberInput>
+          </Box>
+        </Flex>
+        <div>Ventricle Rate</div>
+        <Flex justifyContent="center" alignItems="center">
+          <Box>
+            <NumberInput
+              value={ventricleRate}
+              onChange={(valueString) => setVentricleRate(valueString)}
+              width="200px"
+            >
+              <NumberInputField />
+            </NumberInput>
+          </Box>
+        </Flex>
+        <Button className="btn" onClick={handleSearchClick}>
+          Search
+        </Button>
+      </form>
     </div>
   );
 };
